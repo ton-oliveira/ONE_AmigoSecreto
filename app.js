6bf20e6ec9ed83133f9b6f06637a1e524b24cdaf
ton-oliveira <everton_soliveira@outlook.com>
2025-01-22 02:24:27 -0300

    Criado a lista de amigos e mostra na tela.

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigoSecretos = [];
+
+function adicionarAmigo() {
+    let nome = document.getElementById('amigo').value;
+    if(nome.length < 2){
+        alert("Por favor. Entre com um nome válido");
+        document.getElementById('amigo').focus();
+    }else{
+        amigoSecretos.push(nome);
+        textoNaTela();
+    }      
+}
+
+let mark;
+function exibirLista(lista) {
+    mark += "<li>" + lista + "</li>"        
+}
+
+function textoNaTela(){
+    mark = '';
+    amigoSecretos.forEach(exibirLista);
+    document.getElementById('listaAmigos').innerHTML = mark;
+}
