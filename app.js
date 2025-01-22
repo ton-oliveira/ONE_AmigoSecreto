9c9a3704134d94a130011c71b7932f477df2710c
ton-oliveira <everton_soliveira@outlook.com>
2025-01-22 18:55:27 -0300

    começo func sorterAmigo

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigoSecretos = [];
+let listaNomeSorteados = [];
 
 function adicionarAmigo() {
     let nome = document.getElementById('amigo').value;
     if(nome.length < 2){
         alert("Por favor. Entre com um nome válido");
-        document.getElementById('amigo').focus();
+        limparTela()
+        
     }else{
         amigoSecretos.push(nome);
         textoNaTela();
+        limparTela();
     }      
 }
 
@@ -21,4 +24,25 @@ function textoNaTela(){
     mark = '';
     amigoSecretos.forEach(exibirLista);
     document.getElementById('listaAmigos').innerHTML = mark;
+}
+
+function limparTela(){
+    nome = document.getElementById('amigo');
+    nome.value = '';
+    document.getElementById('amigo').focus()
+}
+
+function sortearAmigo() {
+    let indexLista = parseInt(Math.random() * amigoSecretos.length);
+    console.log("numero Escplhido " + amigoSecretos.length)
+
+    if(listaNomeSorteados.includes(indexLista)){
+        return sortearAmigo();
+    }else{
+        listaNomeSorteados.push(indexLista);
+        console.log(indexLista);
+        return indexLista;
+    }
+
+    
 }
