21d65ede7ccea9b7097dd79962c04f8ac80c2efa
ton-oliveira <everton_soliveira@outlook.com>
2025-01-23 01:06:11 -0300

    Desafio Concluido

--- app.js
+++ app.js
@@ -1,48 +1,56 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigoSecretos = [];
-let listaNomeSorteados = [];
+let listaIndex = [];
 
 function adicionarAmigo() {
     let nome = document.getElementById('amigo').value;
     if(nome.length < 2){
         alert("Por favor. Entre com um nome válido");
-        limparTela()
+        limparTela();
         
     }else{
         amigoSecretos.push(nome);
-        textoNaTela();
+        textoNaTela('listaAmigos', amigoSecretos);
         limparTela();
     }      
 }
 
 let mark;
 function exibirLista(lista) {
-    mark += "<li>" + lista + "</li>"        
+    mark += "<li>" + lista + "</li>" ;       
 }
 
-function textoNaTela(){
+function textoNaTela(id, listas){
     mark = '';
-    amigoSecretos.forEach(exibirLista);
-    document.getElementById('listaAmigos').innerHTML = mark;
+    listas.forEach(exibirLista);
+    document.getElementById(id).innerHTML = mark;
 }
 
 function limparTela(){
     nome = document.getElementById('amigo');
     nome.value = '';
-    document.getElementById('amigo').focus()
+    document.getElementById('amigo').focus();
+}
+
+function resultaNaTela(nome) {
+    document.getElementById('listaAmigos').innerHTML = '';
+    mark = '';
+    exibirLista(`O amigo secreto sorteado é: ${nome}`);
+    document.getElementById('resultado').innerHTML = mark;    
 }
 
 function sortearAmigo() {
     let indexLista = parseInt(Math.random() * amigoSecretos.length);
-    console.log("numero Escplhido " + amigoSecretos.length)
-
-    if(listaNomeSorteados.includes(indexLista)){
-        return sortearAmigo();
-    }else{
-        listaNomeSorteados.push(indexLista);
-        console.log(indexLista);
-        return indexLista;
-    }
-
     
+    if(listaIndex.length == amigoSecretos.length){
+        alert("Lista de Nome Vazia | Já Sorteados");
+
+    }else{
+        if(listaIndex.includes(indexLista)){
+            return sortearAmigo();
+        }else{
+            listaIndex.push(indexLista);
+            resultaNaTela(amigoSecretos[indexLista]);
+        }
+    }
 }
